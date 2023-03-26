755f994e0b9b3462aba99ee1456c7a2d81c9e41d
Alan P. Santos <88924525+Alanps31@users.noreply.github.com>
2023-03-26 19:34:08 -0300

    Update readme.mk

--- readme.mk
+++ readme.mk
@@ -4,9 +4,9 @@ Projeto para desenvolvimento de uma página de supermercado.
 
 1.0 Motivação
 
-	O presente projeto nasceu da necessidade de finalizar um trabalho de conclusão do curso de Gestão em T.i da
+    O presente projeto nasceu da necessidade de finalizar um trabalho de conclusão do curso de Gestão em T.i da
     Universidade Cruzeiro do Sul. A ideia da aplicação é realizar o gerênciamento de um estabelecimento comercial,
-	para isso o presente projeto irá sempre que possível observar as etapas de construção da aplicação:
+    para isso o presente projeto irá sempre que possível observar as etapas de construção da aplicação:
 
     planejamento: na qual a equipe conversa com o cliente e analisa a ideia
     análise de requisitos: validação das funcionalidades que devem ser implementadas
