fb05bc0ac2fcc2a7a243ba3b3f64c669e00431a8
Alanpds <alan.pinheirodossantos@gmail.com>
2023-03-26 19:27:53 -0300

    readme 0.2

--- readme.mk
+++ readme.mk
@@ -1,3 +1,17 @@
 Projeto para desenvolvimento de uma página de supermercado.
 
 #Informações Gerais
+
+1.0 Motivação
+
+	O presente projeto nasceu da necessidade de finalizar um trabalho de conclusão do curso de Gestão em T.i da
+    Universidade Cruzeiro do Sul. A ideia da aplicação é realizar o gerênciamento de um estabelecimento comercial,
+	para isso o presente projeto irá sempre que possível observar as etapas de construção da aplicação:
+
+    planejamento: na qual a equipe conversa com o cliente e analisa a ideia
+    análise de requisitos: validação das funcionalidades que devem ser implementadas
+    modelagem: criação de modelos e representações que ajudam a esquematizar e ordenar o projeto
+    desenvolvimento: codificação propriamente dita, em busca do que foi planejado e modelado
+    testes: validação da aplicação em busca de erros
+    implantação: momento de colocar o app no ar e liberar para usuários
+    monitoramento: hora de observar o funcionamento e levantar ideias de melhorias
