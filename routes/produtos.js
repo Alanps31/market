c49ab43eb320350a4138502ea87634df0b1c0b8c
Alanpds <alan.pinheirodossantos@gmail.com>
2023-03-27 15:45:44 -0300

    login v0.3

--- routes/produtos.js
+++ routes/produtos.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 
 router.get('/',(req,res) => {
-    res.sendFile(paths.views + "/login.html")
+    res.send("Hello There")
 })
 
 router.get('/usuarios',(req, res) => {
